120dd523d6a2bf5ed47a8c4be919e4a12140423c
j0v0057 <javier.vera@walmart.com>
2020-01-18 18:05:06 -0300

    Fix(ResourceGroup Variable Fix)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_availability_set" "nodes" {
-  name                        = "${var.cluster_name}-${var.environment}-${var.name_suffix}-nodes"
+  name                        = "${var.cluster-name}-${var.environment}-${var.name-suffix}-nodes"
   location                    = "${data.azurerm_resource_group.main.location}"
   resource_group_name         = "${data.azurerm_resource_group.main.name}"
   managed                     = true
@@ -7,20 +7,20 @@ resource "azurerm_availability_set" "nodes" {
 }
 
 data "azurerm_resource_group" "main" {
-  name = "${var.main_resource_group}"
+  name = "${var.main-resource-group}"
 }
 
 data "azurerm_subnet" "subnet" {
-  name                 = "${var.subnet_name}"
-  virtual_network_name = "${var.vnet_name}"
-  resource_group_name  = "${var.main_resource_group}"
+  name                 = "${var.subnet-name}"
+  virtual_network_name = "${var.vnet-name}"
+  resource_group_name  = "${var.main-resource-group}"
 }
 data "azurerm_image" "k8s" {
-  name                = "${var.k8s_image_name}"
-  resource_group_name = "${var.images_resource_group}"
+  name                = "${var.k8s-image-name}"
+  resource_group_name = "${var.images-resource-group}"
 }
 
 data "azurerm_image" "bastion" {
-  name                = "${var.bastion_image_name}"
-  resource_group_name = "${var.images_resource_group}"
+  name                = "${var.bastion-image-name}"
+  resource_group_name = "${var.images-resource-group}"
 }
